0837aaefb359acb4e6ea9ac15787ba9c3d015df5
Eric-Gordon <erickgordon96@gmail.com>
2019-02-15 16:02:08 -0500

    cleaned up the code a bit

--- NQueens.py
+++ NQueens.py
@@ -6,9 +6,19 @@ import math
 
 def place_queens(size):
     # max collisions allowed on generation
-    c_range = 50
+    c_range = 0
+    if size > 1000000:
+        c_range = 100
+    elif size > 100000:
+        c_range = 80
+    elif size > 10000:
+        c_range = 50
+    elif size > 1000:
+        c_range = 40
+    else:
+        c_range = 30
+
     # initialize
-    global taken_negatives, taken_positives
     taken_negatives = {}
     taken_positives = {}
     free_cols = list(range(size))
@@ -32,50 +42,38 @@ def place_queens(size):
     return board
 
 
-def get_diagonals(play_board):
-    nd, pd = {}, {}
-    for row in range(0, len(play_board)):
-        if(row - play_board[row]) in difference:
-            if (row - play_board[row]) not in taken_negatives:
-                taken_negatives[(row - play_board[row])] = [row]
+def compute_collisions(board):
+    total, nd, pd = 0, {}, {}
+    for row in range(len(board)):
+        diff = row - board[row]
+        summ = row + board[row]
+        if diff in difference:
+            if diff in nd:
+                nd[diff].append(row)
+                total += 1
             else:
-                taken_negatives[(row - play_board[row])].append(row)
-            if (row - play_board[row]) not in nd:
-                nd[(row - play_board[row])] = 1
+                nd[diff] = [row]
+        if summ in summation:
+            if summ in pd:
+                pd[summ].append(row)
+                total += 1
             else:
-                nd[(row - play_board[row])] += 1
-        if(row + play_board[row]) in summation:
-            if (row + play_board[row]) not in taken_positives:
-                taken_positives[(row + play_board[row])] = [row]
-            else:
-                taken_positives[(row + play_board[row])].append(row)
-            if (row + play_board[row]) not in pd:
-                pd[(row + play_board[row])] = 1
-            else:
-                pd[(row + play_board[row])] += 1
-    return [{k: v for k, v in nd.items() if v > 1}, {k: v for k, v in pd.items() if v > 1}]
-
-
-def compute_collisions(nd, pd):
-    total = 0
-    for diagonal in nd:
-        total += (nd[diagonal] - 1)
-
-    for diagonal in pd:
-        total += (pd[diagonal] - 1)
-    return total
+                pd[summ] = [row]
+    return [total, nd, pd]
 
 
 def compute_attacks(nd, pd):
     under_attack = []
     for diagonal in nd:
-        for row in taken_negatives[diagonal]:
-            if row not in under_attack:
-                under_attack.append(row)
+        if len(nd[diagonal]) > 1:
+            for row in nd[diagonal]:
+                if row not in under_attack:
+                    under_attack.append(row)
     for diagonal in pd:
-        for row in taken_positives[diagonal]:
-            if row not in under_attack:
-                under_attack.append(row)
+        if len(pd[diagonal]) > 1:
+            for row in pd[diagonal]:
+                if row not in under_attack:
+                    under_attack.append(row)
     return under_attack
 
 
@@ -121,63 +119,46 @@ def swap_ok(i, j, play_board):
         return False
 
 
-def perform_swap(i, j, play_board):
-    board = play_board[:]
-    board[i], board[j] = board[j], board[i]
-    taken_negatives, taken_positives = {}, {}
-    diagonals = get_diagonals(board)
-    return [board, diagonals]
-
-
 def solve(size):
     collisions = -1
     global difference, summation
     difference = list(range((size-2), ((size-1) * -1), -1))
     summation = list(range(1, ((size-1)+(size-1))))
-    c1 = 0.6
-    c2 = 32
 
     while collisions != 0:
         board = place_queens(size)
-        diagonals = get_diagonals(board)
-        collisions = compute_collisions(diagonals[0], diagonals[1])
-        attack = compute_attacks(diagonals[0], diagonals[1])
-        number_of_attacks = len(attack)
-
-        limit = c1 * collisions
-
+        compute = compute_collisions(board)
+        collisions = compute[0]
+        nd, pd = compute[1], compute[2]
+        attack = compute_attacks(nd, pd)
+        limit = 0.8 * collisions
         loop = 0
-        while loop < (c2 * size) and collisions != 0:
+        while loop < (32 * size) and collisions != 0:
+            # print("UPDATED: collisions: {}".format(collisions))
             k = 0
-            while k < number_of_attacks and collisions != 0:
+            while k < len(attack):
                 i = attack[k]
-                j = random.randint(0, size-1)
+                j = random.randint(0, size - 1)
                 if swap_ok(i, j, board):
-                    swap = perform_swap(i, j, board)
-                    board = swap[0]
-                    collisions = compute_collisions(swap[1][0], swap[1][1])
+                    board[i], board[j] = board[j], board[i]
+                    compute = compute_collisions(board)
+                    collisions = compute[0]
+                    nd, pd = compute[1], compute[2]
                     if collisions == 0:
                         break
                     if collisions < limit:
-                        limit = c1 * collisions
-                        attack = compute_attacks(swap[1][0], swap[1][1])
-                        number_of_attacks = len(attack)
-
+                        limit = 0.8 * collisions
+                        attack = compute_attacks(nd, pd)
                 k += 1
-            loop += number_of_attacks
+            loop += len(attack)
     return board
 
 
 solutions = []
 queens_file = open("nqueens.txt")
 for n in queens_file:
-    start = time.time()
-    print("n = {}".format(int(n)))
     solution = solve(int(n))
     solutions.append(solution)
-    print("Solution Found in: {:.10f}\n".format(time.time()-start))
-    print("******")
-    print()
 queens_file.close()
 
 solution_file = open("nqueens_out.txt", "w")
--- random_queens.py
+++ random_queens.py
@@ -1,8 +1,8 @@
 import random
-min_range = [4, 100, 1000, 10000, 100000]
-max_range = [99, 999, 9999, 99999, 999999]
+min_range = [4, 100, 1000, 10000]
+max_range = [99, 999, 9999, 99999]
 queens_file = open("nqueens.txt", "w")
-for i in range(5):
+for i in range(4):
     for j in range(4):
         queens_file.write("{}\n".format(
             random.randint(min_range[i], max_range[i])))
